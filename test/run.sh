ef1bceb890c7dd06c99eac0ac64eb8eae1c07213
Janne Valkealahti <janne.valkealahti@gmail.com>
2014-03-26 17:32:40 +0000

    initial import

---
+++ test/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cd $(dirname $0)
+
+cd ../complete
+./gradlew clean build
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+
+./gradlew clean
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+
+exit
